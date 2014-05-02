84ce39dc6fbbb2d751ddf6fd85a963e7a4ca6b0e
Sam <samcooke98@yahoo.com.au>
2014-05-02 17:18:11 +1000

    [Added] Basic Garry's Mod gamemode files

---
+++ gamemode/init.lua
@@ -0,0 +1,10 @@
+
+--Include files here
+include("shared.lua")
+
+
+
+
+--Add Clientside files here
+AddCSLuaFile( "cl_init.lua" )
+AddCSLuaFile( "shared.lua" )
---
+++ gamemode/shared.lua
@@ -0,0 +1,8 @@
+GM.Name 	= "Spacebuild Infinite"
+GM.Author 	= "Samuel Cooke"
+GM.Email 	= "Don't" 
+
+
+function GM:Initialize()
+
+end
